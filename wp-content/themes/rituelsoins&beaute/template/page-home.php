deefc8a1fa21869d25441b5e281c956c2c3c96f4
Klaimand <clement.michellod@gmail.com>
2024-02-05 14:41:31 +0100

    Mise à jour wordpress

--- wp-content/themes/rituelsoins&beaute/template/page-home.php
+++ wp-content/themes/rituelsoins&beaute/template/page-home.php
@@ -1,4 +1,4 @@
-<?php /* Template Name: Accueil */
+<?php /* Template Name: Accueil-Home */
 get_header();
 ?>
 
@@ -7,15 +7,14 @@ get_header();
     <?php
     $image = get_field('background-logo');
     if (!empty($image)) : ?>
-    <img class="background-logo" src="<?php echo esc_url($image['url']); ?>"
-        alt="<?php echo esc_attr($image['alt']); ?>" />
+        <img class="background-logo" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
     <?php endif;
 
     $image = get_field('background-image');
     if (!empty($image)) : ?>
-    <video class="background-image" autoplay muted loop>
-        <source src="<?php echo esc_url($image['url']); ?>" type="video/mp4">
-    </video>
+        <video class="background-image" autoplay muted loop>
+            <source src="<?php echo esc_url($image['url']); ?>" type="video/mp4">
+        </video>
     <?php endif; ?>
 
 
@@ -37,16 +36,14 @@ get_header();
         <div class="services-item">
             <?php $image = get_field('service-1-logo');
             if (!empty($image)) : ?>
-            <img var(--color-text-green); src="<?php echo esc_url($image['url']); ?>"
-                alt="<?php echo esc_attr($image['alt']); ?>" />
+                <img var(--color-text-green); src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
             <?php endif; ?>
             <h3><?php the_field('service-1-description'); ?></h3>
         </div>
         <div class="services-item">
             <?php $image = get_field('service-2-logo');
             if (!empty($image)) : ?>
-            <img var(--color-text-green); src="<?php echo esc_url($image['url']); ?>"
-                alt="<?php echo esc_attr($image['alt']); ?>" />
+                <img var(--color-text-green); src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
             <?php endif; ?>
             <h3><?php the_field('service-2-description'); ?></h3>
 
@@ -54,8 +51,7 @@ get_header();
         <div class="services-item">
             <?php $image = get_field('service-3-logo');
             if (!empty($image)) : ?>
-            <img var(--color-text-green); src="<?php echo esc_url($image['url']); ?>"
-                alt="<?php echo esc_attr($image['alt']); ?>" />
+                <img var(--color-text-green); src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
             <?php endif; ?>
             <h3><?php the_field('service-3-description'); ?></h3>
 
@@ -69,31 +65,30 @@ get_header();
 <div class="services-grid">
     <div class="services-grid-container">
         <?php for ($i = 1; $i <= 4; $i++) : ?>
-        <div class="services-grid-item">
-            <?php $image = get_field("service-grid{$i}-image"); ?>
-            <?php if (!empty($image)) : ?>
-            <div class="services-grid-img" style="background-image: url('<?php echo esc_url($image['url']); ?>')">
-                <?php else : ?>
-                <div class="services-grid-img">
-                    <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-                    <?php endif; ?>
-                    <div class="services-grid-hover ">
-                        <h3><?php the_field("service-grid{$i}-title"); ?></h3>
-                        <div class="grid">
-                            <div class="grid-inner">
+            <div class="services-grid-item">
+                <?php $image = get_field("service-grid{$i}-image"); ?>
+                <?php if (!empty($image)) : ?>
+                    <div class="services-grid-img" style="background-image: url('<?php echo esc_url($image['url']); ?>')">
+                    <?php else : ?>
+                        <div class="services-grid-img">
+                            <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                        <?php endif; ?>
+                        <div class="services-grid-hover ">
+                            <h3><?php the_field("service-grid{$i}-title"); ?></h3>
+                            <div class="grid">
+                                <div class="grid-inner">
 
-                                <p><?php the_field("service-grid{$i}-description"); ?></p>
-                                <a class="button button-pink"
-                                    href="<?php echo get_home_url() . the_field("service-grid{$i}-title"); ?>">Voir nos
-                                    soins
-                                    <?php the_field("service-grid{$i}-title"); ?></a>
+                                    <p><?php the_field("service-grid{$i}-description"); ?></p>
+                                    <a class="button button-pink" href="<?php echo get_home_url() . the_field("service-grid{$i}-title"); ?>">Voir nos
+                                        soins
+                                        <?php the_field("service-grid{$i}-title"); ?></a>
+                                </div>
                             </div>
                         </div>
+                        </div>
                     </div>
-                </div>
+                <?php endfor; ?>
             </div>
-            <?php endfor; ?>
-        </div>
     </div>
 </div>
 
