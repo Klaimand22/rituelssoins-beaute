21690613044a1e47dc2b665c81c93c1f95d76e4d
Klaimand22 <clement.michellod@gmail.com>
2024-02-13 08:49:14 +0100

    autoplay video iphone

--- wp-content/themes/rituelsoins&beaute/template/page-home.php
+++ wp-content/themes/rituelsoins&beaute/template/page-home.php
@@ -3,22 +3,21 @@ get_header();
 ?>
 
 <script>
-AOS.init();
+    AOS.init();
 </script>
 
 <div class="container-background">
     <?php
     $image = get_field('background-logo');
     if (!empty($image)) : ?>
-    <img data-aos="zoom-out" class="background-logo" src="<?php echo esc_url($image['url']); ?>"
-        alt="<?php echo esc_attr($image['alt']); ?>" />
+        <img data-aos="zoom-out" class="background-logo" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
     <?php endif;
 
     $image = get_field('background-image');
     if (!empty($image)) : ?>
-    <video class="background-image" autoplay muted loop>
-        <source src="<?php echo esc_url($image['url']); ?>" type="video/mp4">
-    </video>
+        <video class="background-image" autoplay muted loop playsinline>
+            <source src="<?php echo esc_url($image['url']); ?>" type="video/mp4">
+        </video>
     <?php endif; ?>
 
 
@@ -41,16 +40,14 @@ AOS.init();
         <div class="services-item">
             <?php $image = get_field('service-1-logo');
             if (!empty($image)) : ?>
-            <img var(--color-text-green); src="<?php echo esc_url($image['url']); ?>"
-                alt="<?php echo esc_attr($image['alt']); ?>" />
+                <img var(--color-text-green); src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
             <?php endif; ?>
             <h3><?php the_field('service-1-description'); ?></h3>
         </div>
         <div class="services-item">
             <?php $image = get_field('service-2-logo');
             if (!empty($image)) : ?>
-            <img var(--color-text-green); src="<?php echo esc_url($image['url']); ?>"
-                alt="<?php echo esc_attr($image['alt']); ?>" />
+                <img var(--color-text-green); src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
             <?php endif; ?>
             <h3><?php the_field('service-2-description'); ?></h3>
 
@@ -58,8 +55,7 @@ AOS.init();
         <div class="services-item">
             <?php $image = get_field('service-3-logo');
             if (!empty($image)) : ?>
-            <img var(--color-text-green); src="<?php echo esc_url($image['url']); ?>"
-                alt="<?php echo esc_attr($image['alt']); ?>" />
+                <img var(--color-text-green); src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
             <?php endif; ?>
             <h3><?php the_field('service-3-description'); ?></h3>
 
@@ -73,27 +69,27 @@ AOS.init();
 <div class="services-grid">
     <div class="services-grid-container">
         <?php if (have_rows('service_card')) : ?>
-        <?php while (have_rows('service_card')) : the_row(); ?>
-        <div class="services-grid-item" data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="1500">
-            <?php $image = get_sub_field('image'); ?>
-            <div class="services-grid-img" style="background-image: url('<?php echo esc_url($image['url']); ?>')">
-                <?php if (empty($image)) : ?>
-                <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-                <?php endif; ?>
-                <div class="services-grid-hover">
-                    <h3><?php the_sub_field('title'); ?></h3>
-                    <div class="grid">
-                        <div class="grid-inner">
-                            <p><?php the_sub_field('description'); ?></p>
-                            <a class="button button-pink" href="<?php echo the_sub_field('link'); ?>">
-                                Voir nos <?php the_sub_field('title'); ?>
-                            </a>
+            <?php while (have_rows('service_card')) : the_row(); ?>
+                <div class="services-grid-item" data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="1500">
+                    <?php $image = get_sub_field('image'); ?>
+                    <div class="services-grid-img" style="background-image: url('<?php echo esc_url($image['url']); ?>')">
+                        <?php if (empty($image)) : ?>
+                            <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                        <?php endif; ?>
+                        <div class="services-grid-hover">
+                            <h3><?php the_sub_field('title'); ?></h3>
+                            <div class="grid">
+                                <div class="grid-inner">
+                                    <p><?php the_sub_field('description'); ?></p>
+                                    <a class="button button-pink" href="<?php echo the_sub_field('link'); ?>">
+                                        Voir nos <?php the_sub_field('title'); ?>
+                                    </a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        <?php endwhile; ?>
+            <?php endwhile; ?>
         <?php endif; ?>
     </div>
 </div>
